df5765a883d57f4189ecc3ee9250ae549781bac9
pvisser82 <visser.paul@gmail.com>
2017-12-30 23:28:25 +0200

    Updated vignetted to display static images and not interactive maps due to fact that it does not work properly in browser

--- vignettes/earthquakedata.R
+++ vignettes/earthquakedata.R
@@ -2,8 +2,8 @@
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
-  fig.width = 7,
-  fig.height = 7
+  fig.width = 7
+  
 )
 library(earthquakedata)
 library(dplyr)
@@ -11,29 +11,4 @@ library(ggplot2)
 library(leaflet)
 
 
-
-## ---- message=FALSE------------------------------------------------------
-readr::read_delim("../earthquakes.tsv.gz",delim = "\t") %>%
-  eq_clean_data() %>%
-  dplyr::filter(COUNTRY == c("USA","MEXICO") & lubridate::year(DATE) >= 2000) %>%
-  ggplot() +
-    geom_timeline(aes(x = DATE, y = COUNTRY,size = EQ_PRIMARY, colour = DEATHS)) +
-    geom_timeline_label(aes(x = DATE, y = COUNTRY, label = LOCATION_NAME, size = EQ_PRIMARY), n_max = 5) +
-    ggtitle("Earthquake Timeline") +
-    theme_timeline() +
-    labs(size = "Richter Scale value:", colour = "# of Deaths:")
-
-## ---- message=FALSE------------------------------------------------------
-
- readr::read_delim("earthquakes.tsv.gz", delim = "\t") %>%
-  eq_clean_data() %>%
-  dplyr::filter(COUNTRY == "MEXICO" & lubridate::year(DATE) >= 2000) %>%
-  eq_map(annot_col = "DATE")
-
-## ---- message=FALSE------------------------------------------------------
-readr::read_delim("earthquakes.tsv.gz",delim = "\t") %>%
-  eq_clean_data() %>%
-  dplyr::filter(COUNTRY == "MEXICO" & lubridate::year(DATE) >= 2000) %>%
-  dplyr::mutate(popup_text = eq_create_label(.))%>%
-  eq_map(annot_col = "popup_text")
 
